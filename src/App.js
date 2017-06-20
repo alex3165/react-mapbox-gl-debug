77d1a5c514481e7903132ba0edfe81deea2eb6f1
Alexandre Rieux <alexr.3165@gmail.com>
2017-06-20 14:45:48 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React, { Component } from 'react';
 import ReactMap from 'react-mapbox-gl';
 
-const accessToken = "pk.eyJ1IjoiYWxleDMxNjUiLCJhIjoiY2l4b3V0Z3RpMDAxczJ4cWk2YnEzNTVzYSJ9.MFPmOyHy8DM5_CVaqPYhOg";
+const accessToken = "pk.eyJ1IjoiYWxleDMxNjUiLCJhIjoiY2o0MHp2cGtiMGFrajMycG5nbzBuY2pjaiJ9.QDApU0XH2v35viSwQuln5w";
 const style = "mapbox://styles/mapbox/streets-v9";
+
+const Map = ReactMap({
+  accessToken
+});
 
 const mapStyle = {
   height: '100vh',
   width: '100vw'
-}
+};
 
 class App extends Component {
   render() {
     return (
-      <ReactMap
+      <Map
         style={style}
-        accessToken={accessToken}
         containerStyle={mapStyle}
       />
     );
